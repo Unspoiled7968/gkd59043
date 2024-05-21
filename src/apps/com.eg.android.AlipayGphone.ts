adc07b05576815abab01545e503fc48ecd8be9d3
Unspoiled7968 <Kaileertakaig7@gmail.com>
2024-05-21 04:52:34 +0000

    1

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -5,17 +5,15 @@ export default defineGkdApp({
   name: '支付宝',
   groups: [
     {
-      key: 0,
-      name: '名下其他账号花呗恢复使用X',
-      rules: {
-        matches:
-          '@View + [text="你名下其他账号的花呗暂时无法使用，完成确认可恢复使用"]',
-      },
-      activityIds: [
-        'com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main',
+      key: 1,
+      name: '[ChangeMe]规则名称-2024-05-21 12:49:27',
+      desc: '[ChangeMe]本规则由GKD网页端审查工具生成',
+      rules: [
+        {
+          activityIds: 'com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main',
+          matches: '@View + [text="你名下其他账号的花呗暂时无法使用，完成确认可恢复使用"]',
+        },
       ],
-      matchDelay: 500,
-      matchTime: 5000,
     },
   ],
 });
