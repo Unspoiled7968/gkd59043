369d68eb0c774ecffcaf04e221c7ecc6794b29fb
Unspoiled7968 <Kaileertakaig7@gmail.com>
2024-05-21 06:08:14 +0000

    1

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -6,35 +6,21 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '汽水音乐看广告结束自动关闭',
-      desc: '点击领取成功',
+      name: '汽水音乐看广告领取vip',
+      desc: '多',
       rules: [
         {
           quickFind: true,
           activityIds: 'com.ss.android.excitingvideo.ExcitingVideoActivity',
           matches: '[text="领取成功，关闭，按钮"]',
         },
-      ],
-    },
-    {
-      key: 2,
-      name: '汽水音乐看广告结束自动关闭领取奖励',
-      desc: '点击领取奖励',
-      rules: [
         {
           quickFind: true,
           activityIds: 'com.ss.android.excitingvideo.ExcitingVideoActivity',
           matches: '[text="领取奖励"]',
         },
-      ],
-    },
-    {
-      key: 3,
-      name: '汽水音乐解锁第N天 畅听',
-      desc: '点击立即解锁',
-      rules: [
         {
-          quickFind: true,
+          quickFind: true,        
           activityIds: 'com.ss.android.excitingvideo.ExcitingVideoActivity',
           matches: '[text~="立即解锁 第.*天畅听"]',
         },
