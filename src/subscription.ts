d7929d0cf7e44950b538c22e0afbcb22c7eb41d2
Unspoiled7968 <Kaileertakaig7@gmail.com>
2024-05-21 04:07:48 +0000

    修改：     src/subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
-  checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  id: 59043,
+  name: 'gkd59043-Subscription',
+  version: 1.0,
+  author: 'Unspoiled7968',
+  checkUpdateUrl: './gkd59043.version.json5',
+  supportUri: 'https://github.com/Unspoiled7968/gkd59043',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
