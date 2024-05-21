9c7dc9f0dc0300c28df5b57fe71e082296265e23
Unspoiled7968 <Kaileertakaig7@gmail.com>
2024-05-21 11:17:14 +0800

    Update com.eg.android.AlipayGphone.ts

--- src/app/com.eg.android.AlipayGphone.ts
+++ src/app/com.eg.android.AlipayGphone.ts
@@ -3,5 +3,13 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.eg.android.AlipayGphone',
   name: '支付宝',
-  groups: [],
+  groups: [
+    {
+      key: 0,
+      name: '名下其他账号花呗恢复使用X',
+      rules: { matches: '*+[text="你名下其他账号的花呗暂时无法使用，完成确认可恢复使用"]' },
+      snapshotUrls: ['https://i.gkd.li/i/13070251'],
+      activityIds?: IArray<com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main>,
+    },
+  ],
 });
