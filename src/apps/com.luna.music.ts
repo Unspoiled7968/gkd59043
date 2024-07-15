11270d6422ef86c2a1fb5de8023d66f8576044c6
Unspoiled7968 <Kaileertakaig7@gmail.com>
2024-07-15 03:31:58 +0000

    1

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -32,6 +32,12 @@ export default defineGkdApp({
           activityIds: 'com.ss.android.excitingvideo.ExcitingVideoActivity',
           matches: '[text~="立即解锁.*畅听"]',
         },
+        {
+          quickFind: true,
+          action: 'clickCenter',
+          activityIds: 'com.ss.android.excitingvideo.ExcitingVideoActivity',
+          matches: '[text~=".*解锁.*畅听"]',
+        },
       ],
     },
   ],
