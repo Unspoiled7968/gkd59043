03364c815267eccac1ad266241f192d0898529af
Unspoiled7968 <Kaileertakaig7@gmail.com>
2024-10-16 03:47:17 +0000

    1

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -25,10 +25,7 @@ export default defineGkdApp({
         },
         {
           action: 'clickCenter',
-          activityIds: [
-            'com.luna.biz.main.main.MainActivity',
-            'com.ss.android.excitingvideo.ExcitingVideoActivity',
-          ],
+          activityIds: ['com.luna.biz.main.main.MainActivity','com.ss.android.excitingvideo.ExcitingVideoActivity'],
           matches: '[text~=".*解锁.*畅听"]',
         },
       ],
