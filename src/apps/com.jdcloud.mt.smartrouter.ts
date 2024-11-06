f84e3e366c8eb78d79e67372d43ebf739e287fe9
Unspoiled7968 <Kaileertakaig7@gmail.com>
2024-11-06 10:37:29 +0000

    1

---
+++ src/apps/com.jdcloud.mt.smartrouter.ts
@@ -0,0 +1,62 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.jdcloud.mt.smartrouter',
+  name: '京东云无限宝',
+  groups: [
+    {
+      key: 5,
+      name: '激励广告领金豆',
+      desc: '多',
+      rules: [
+        {
+          action: 'clickCenter',
+          activityIds: [
+            '.newapp.activity.MainActivity',
+            'com.qq.e.ads.PortraitADActivity',
+          ],
+          matches: '[text="继续赚京豆"]',
+
+        },
+        {
+          action: 'clickCenter',
+          activityIds: [
+            '.newapp.activity.MainActivity',
+            'com.qq.e.ads.PortraitADActivity',
+          ],
+          matches: '[text="点击广告拿奖励"]',
+
+        },
+        {
+          action: 'clickCenter',
+          activityIds: [
+            'com.qq.e.ads.PortraitADActivity',
+            '.newapp.activity.MainActivity',
+          ],
+          matches: '@ImageView <<(3) FrameLayout >(3) [text="恭喜获得奖励"]',
+
+        },
+        {
+          action: 'clickCenter',
+          desc: '看广告×',
+          activityIds: [
+            'com.qq.e.ads.PortraitADActivity',
+            '.newapp.activity.MainActivity',
+          ],
+          matches: '@FrameLayout - LinearLayout >2 [text="恭喜获得奖励"]',
+
+        },
+        {
+          action: 'clickCenter',
+          activityIds: [
+            'com.qq.e.ads.PortraitADActivity',
+            '.newapp.activity.MainActivity',
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          ],
+          matches: 'ImageView[vid="iv_bean"] +4 [text="继续赚京豆"]',
+
+        },
+      ],
+    },
+  ],
+});
---
+++ src/apps/om.miui.securitycenter.ts
@@ -0,0 +1,22 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.miui.securitycenter',
+  name: 'hyperos手机管家',
+  groups: [
+    {
+      key: 5,
+      name: '手机管家拒绝京东云无限宝跳转',
+      desc: '拒绝京东云无限宝跳转', 
+      rules: [
+        {
+          action: 'clickCenter',
+          activityIds: [
+            'com.miui.wakepath.ui.ConfirmStartActivity',
+          ],
+          matches: '@* < LinearLayout - ScrollView >5 [text~="(?is)京东云无线宝 想要打开.*"]',
+        },
+      ],
+    },
+  ],
+});
