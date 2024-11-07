38f43302f711cdb03c9de4a80af0f36b116a0066
Unspoiled7968 <Kaileertakaig7@gmail.com>
2024-11-07 08:49:51 +0000

    1

--- src/apps/com.jdcloud.mt.smartrouter.ts
+++ src/apps/com.jdcloud.mt.smartrouter.ts
@@ -5,9 +5,9 @@ export default defineGkdApp({
   name: '京东云无限宝',
   groups: [
     {
-      key: 5,
+      key: 1,
       name: '激励广告领金豆',
-      desc: '多',
+      desc: '切换到wait页自动观看广告',
       rules: [
         {
           action: 'clickCenter',
@@ -17,6 +17,13 @@ export default defineGkdApp({
           ],
           matches: '[text="继续赚京豆"]',
         },
+      ]
+    },
+    {
+      key: 2,
+      name: '激励广告领金豆',
+      desc: '观看广告过程等待结束自动点击关闭',
+      rules: [
         {
           action: 'clickCenter',
           activityIds: [
@@ -44,11 +51,63 @@ export default defineGkdApp({
         {
           action: 'clickCenter',
           activityIds: [
+            '.newapp.activity.MainActivity',
+            'com.qq.e.ads.PortraitADActivity',
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          ],
+          matches: '@ImageView <<3 ViewGroup >2 [text="我要免看本次广告"]',
+
+        },
+        {
+          action: 'clickCenter',
+          activityIds: [
+            '.newapp.activity.MainActivity',
+            'com.qq.e.ads.PortraitADActivity',
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          ],
+          matches: '@ImageView <<3 ViewGroup[name="android.view.ViewGroup"] - ViewGroup >2 [text="免费获取"]',
+
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '激励广告领金豆',
+      desc: '观看单个广告结束是否点击继续',
+      rules: [
+        {
+          action: 'clickCenter',
+          activityIds: [
+            '.newapp.activity.MainActivity',
+            'com.qq.e.ads.PortraitADActivity',
+          ],
+          matches: '[text="继续赚京豆"]',
+        },
+        {
+          action: 'clickCenter',
+          activityIds: [
             'com.qq.e.ads.PortraitADActivity',
             '.newapp.activity.MainActivity',
             'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
           ],
           matches: 'ImageView[vid="iv_bean"] +4 [text="继续赚京豆"]',
+        },
+      ]
+    },
+    {
+      key: 5,
+      name: '激励广告领金豆',
+      desc: '全部观看结束 点击继续赚',
+      rules: [
+        {
+          action: 'clickCenter',
+          activityIds: [
+            '.newapp.activity.MainActivity',
+            'com.qq.e.ads.PortraitADActivity',
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          ],
+          matches: '@Button[text="明天继续赚呀！"] -2 TextView[text="今日观看奖励次数已用完"]',
+
         },
       ],
     },
